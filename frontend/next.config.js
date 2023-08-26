1af8762665cea7d96cca3e0ae778b224324e6701
Alif Mohd-Amir <alifma.96@gmail.com>
2023-08-26 12:53:48 -0400

    init backend with next config for rewrite

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = () => {
+  const rewrites = async () => {
+    return [
+      {
+        source: "/python-api/:path*",
+        destination: "http://127.0.0.1:8000/api/:path*",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+}
 
 module.exports = nextConfig
