0a505f44dbdbcb48ce6dae94ebc64fa79380f7d5
Afif <feefdeez@Afifs-MacBook-Pro.local>
2023-08-26 13:50:07 -0400

    added route for providing a recipe

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,25 @@ def generate_meals(ingredients):
 
     return response
 
-@app("/openai")
+def generate_receipe(selected_meal):
+    response = openai.Completion.create(
+    model="gpt-3.5-turbo-16k",
+    messages=[
+        {
+        "role": "user",
+        "content": f"Provide me a recipe for {selected_meal}"
+        }
+    ],
+    temperature=1,
+    max_tokens=1638,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+
+    return response
+
+@app("/generate-meal")
 def generate_from_openai():
     # List of ingredients you have
     my_ingredients = ["Jello", "honey", "bread", "walnuts", "peanuts", "chia", "seeds", "rice", "flour", "potatoes", "milk", "watermelon", "peanut butter", "greek yogurt", "mushrooms", "broccoli", "apples", "pancetta", "brussel", "sprouts", "sausage", "pork tenderloins"]
@@ -38,3 +56,11 @@ def generate_from_openai():
 
     return recommended_meals 
 
+@app("/provide-receipe")
+def provide_receipe():
+    # Provide recipe based on the selected meal
+    recipe = generate_receipe(selected_meal)
+
+    selected_meal = "Jello"
+
+    
