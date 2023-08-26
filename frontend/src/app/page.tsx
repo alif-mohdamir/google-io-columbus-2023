e8cd0edeb08153f767a94de65788523ee93e5871
conradstephens <conradastephens@gmail.com>
2023-08-26 14:34:45 -0400

    showing recipes section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import BasicModal from "@/components/modal";
 // import Input from "@/components/input";
 import { DeleteOutline } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
 import {
   Button,
   Checkbox,
@@ -14,7 +16,10 @@ import {
   Stack,
   useTheme,
   Input,
+  ListItemText,
+  Modal,
 } from "@mui/material";
+import { useState } from "react";
 import {
   FieldValues,
   FormProvider,
@@ -23,11 +28,13 @@ import {
 } from "react-hook-form";
 
 export default function Home() {
-  const { palette } = useTheme();
+  const [recipes, setRecipes] = useState<string[]>([]);
   const methods = useForm({
     shouldUnregister: true,
     shouldFocusError: true,
   });
+
+  const [loading, setLoading] = useState(false);
 
   const removeIngredient = (index: number) => () => {
     remove(index);
@@ -44,75 +51,129 @@ export default function Home() {
     append({ value: "" });
   };
 
-  const onSubmit = (data: FieldValues) => {
-    console.log("data", data);
+  const startLoading = () => {
+    setLoading(true);
+  };
+
+  const stopLoading = () => {
+    setLoading(false);
+  };
+
+  const onSubmit = async (data: FieldValues) => {
+    try {
+      startLoading();
+      console.log("data", data);
+      const ingredients = data.ingredients.map(
+        ({ value }: { value: string }) => value
+      );
+      console.log("ingredients", ingredients);
+
+      const response = await fetch("/python-path/generate-meal", {
+        method: "POST",
+        body: JSON.stringify({
+          ingredients,
+        }),
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        console.log("response => ", responseData);
+        stopLoading();
+        throw new Error("Network response was not ok");
+      }
+
+      console.log("data => ", responseData);
+      stopLoading();
+    } catch (e) {
+      stopLoading();
+      console.error(e);
+    }
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <div className="max-w-md w-full flex flex-col">
-            <List subheader="Ingredients" className="w-full">
-              {fields.map((field, index) => {
-                const ingredients = methods.formState.errors.ingredients as any;
-                const errorValue = ingredients?.[index]?.value as any;
+      <div className="flex flex-col gap-10">
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <div className="max-w-md w-full flex flex-col">
+              <List subheader="Ingredients" className="w-full">
+                {fields.map((field, index) => {
+                  const ingredients = methods.formState.errors
+                    .ingredients as any;
+                  const errorValue = ingredients?.[index]?.value as any;
 
-                return (
-                  <ListItem key={field.id}>
-                    <ListItemIcon>
-                      <Checkbox />
-                    </ListItemIcon>
-                    <FormControl fullWidth>
-                      <Input
-                        id={field.id}
-                        placeholder="e.g 1 cup of flour"
-                        // variant="standard"
-                        error={Boolean(errorValue)}
-                        {...methods.register(`ingredients.${index}.value`, {
-                          validate: (value) => value !== "",
-                          required: "Ingredient can't be empty",
-                        })}
-                      />
-                      {errorValue && (
-                        <FormHelperText error>
-                          {errorValue?.message}
-                        </FormHelperText>
-                      )}
-                    </FormControl>
+                  return (
+                    <ListItem key={field.id}>
+                      <ListItemIcon>
+                        <Checkbox />
+                      </ListItemIcon>
+                      <FormControl fullWidth>
+                        <Input
+                          id={field.id}
+                          placeholder="e.g 1 cup of flour"
+                          error={Boolean(errorValue)}
+                          {...methods.register(`ingredients.${index}.value`, {
+                            validate: (value) => value !== "",
+                            required: "Ingredient can't be empty",
+                          })}
+                        />
+                        {errorValue && (
+                          <FormHelperText error>
+                            {errorValue?.message}
+                          </FormHelperText>
+                        )}
+                      </FormControl>
 
-                    <IconButton
-                      edge="end"
-                      aria-label="delete"
-                      onClick={removeIngredient(index)}
-                    >
-                      <DeleteOutline />
-                    </IconButton>
-                  </ListItem>
-                );
-              })}
-            </List>
-            <Stack direction="row" spacing={2}>
-              <Button
-                color="secondary"
-                variant="outlined"
-                onClick={addIngredient}
-                sx={{ textTransform: "none" }}
-              >
-                Add ingredient
-              </Button>
-              <Button
-                color="secondary"
-                variant="outlined"
-                type="submit"
-                sx={{ textTransform: "none" }}
-              >
-                Generate recipes
-              </Button>
-            </Stack>
-          </div>
-        </form>
-      </FormProvider>
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={removeIngredient(index)}
+                      >
+                        <DeleteOutline />
+                      </IconButton>
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Stack direction="row" spacing={2}>
+                <Button
+                  color="secondary"
+                  variant="outlined"
+                  onClick={addIngredient}
+                  sx={{ textTransform: "none" }}
+                  disabled={methods.formState.isSubmitting}
+                >
+                  Add ingredient
+                </Button>
+                <LoadingButton
+                  color="secondary"
+                  variant="outlined"
+                  type="submit"
+                  loading={loading}
+                  sx={{ textTransform: "none" }}
+                  disabled={
+                    methods.formState.isSubmitting || !methods.formState.isValid
+                  }
+                >
+                  Generate recipes
+                </LoadingButton>
+              </Stack>
+            </div>
+          </form>
+        </FormProvider>
+        {recipes.length > 0 && (
+          <List subheader="Recipes" className="w-full">
+            {recipes.map((recipe, index) => (
+              <BasicModal
+                key={index}
+                label={`Recipe ${index + 1}`}
+                recipe={recipe}
+              />
+            ))}
+          </List>
+        )}
+      </div>
     </main>
   );
 }
---
+++ frontend/src/components/modal.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { ListItemText } from "@mui/material";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+interface ComponentProps {
+  recipe: string;
+  label: string;
+}
+
+export default function BasicModal(props: ComponentProps) {
+  const { recipe, label } = props;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <ListItemText primary={label} />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description">{recipe}</Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
