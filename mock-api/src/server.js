c5658dd1345adb248e592cf65355a051249dd270
Stephan Rotolante <stephanrotolante@gmail.com>
2023-08-26 12:19:35 -0400

    wip

--- mock-api/src/server.js
+++ mock-api/src/server.js
@@ -1,17 +1,31 @@
 const express = require('express')
 const cors = require('cors')
+const { faker } = require('@faker-js/faker')
 const app = express()
 
-const PORT =  parseInt(process.env['PORT'] ?? '8000')
+
+const PORT = parseInt(process.env['PORT'] ?? '8000')
 
 app.use(cors())
 app.use(express.json())
 
 
-app.get('/hi'  ,(req, res, next) =>{
-  res.json({msg: 'hi mom!'})
+
+app.get("/items", async (_, res) => {
+    const ingredients = new Array(6).map(() => { ingredientName: faker.word.noun() })
+
+
+    await new Promise((res) => setTimeout(res, 3000))
+
+
+    res.json({ ingredients })
+
+})
+
+app.get('/hi', (_, res) => {
+    res.json({ msg: 'hi mom!' })
 })
 
 app.listen(PORT, function () {
-  console.log(`web server listening on port ${PORT}`)
+    console.log(`web server listening on port ${PORT}`)
 })
