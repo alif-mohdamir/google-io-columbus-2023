06168a4ced59ddf21eaac18dc7627bd44fa82714
Alif Mohd-Amir <alifma.96@gmail.com>
2023-08-26 11:31:37 -0400

    Add initial list component

---
+++ frontend/src/components/list.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { List as MuiList, ListItem, ListItemText } from '@mui/material';
+
+
+const List = ({ items }: { items: Array<any> }) => {
+  const placeholderItems = [
+    'item 1', 'item2'
+  ]
+
+  const itemsToRender = items || placeholderItems
+  return (
+    <MuiList sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {
+        itemsToRender.map((item, index) => (
+          <ListItem
+            key={index}
+          >
+            <ListItemText primary={item} />
+          </ListItem>
+        ))
+      }
+
+    </MuiList>
+  );
+
+};
+
+export default List;
